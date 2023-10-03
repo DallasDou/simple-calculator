294b0f76f1b4f48d1c44f172ab59c3ddb78b8979
DallasDou <dallasdouthett@siu.edu>
2023-10-03 12:43:56 -0500

    Implemented division feature.

--- calculator.c
+++ calculator.c
@@ -3,8 +3,7 @@
 double add(double a, double b);
 double subtract(double a, double b);
 double multiply(double a, double b);
-
-
+double divide(double a, double b);
 
 void main(){
     double a, b result;
@@ -19,6 +18,8 @@ void main(){
     result = multiply(a,b);
     printf("Result: %lf\n", result);
 
+    result = divide(a,b);
+    printf("Result: %lf\n", result);
     
 }
 
@@ -32,4 +33,13 @@ double subtract(double a, double b) {
 }
 double multiply(double a, double b) {
     return a * b;
+}
+
+double divide(double a, double b) {
+    if (b != 0) {
+        return a / b;
+    } else {
+        printf("Error: Division by zero.\n");
+        return 0;
+    }
 }
