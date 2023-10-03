4394dabb380e9548977afcc8ea3815fdfa232a3d
DallasDou <dallasdouthett@siu.edu>
2023-10-03 13:25:25 -0500

    Added comments and documentation.

--- calculator.c
+++ calculator.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+//  Function prototypes
 double add(double a, double b);
 double subtract(double a, double b);
 double multiply(double a, double b);
@@ -11,13 +12,16 @@ int main(){
 
     printf("This is a simple calculator program. Please choice an operation:\n1: Add\n2: Subtract\n3: Multiply\n4: Divide\n");
     scanf("%d", &c);
-
+    
+    //  Checks if choice input is valid.
     if (c >= 5 || c<=0){
         printf("Invalid input. Enter a valid operation (1-4)");
         return 0;}
     printf("Please enter two numbers: ");
     scanf("%lf %lf", &a, &b);
     
+    //  If choice is valid:
+    //  Program determines which operation was selected and prints result of the operations.
     if (c==1){
     result = add(a, b);
     printf("Result: %lf", result);}
@@ -37,6 +41,7 @@ int main(){
     return 0;
 }
 
+// Function logic
 double add(double a, double b) {
     return a + b;
 }
@@ -52,7 +57,7 @@ double divide(double a, double b) {
     if (b != 0) {
         return a / b;
     } else {
-        printf("Error: Division by zero.\n");
+        printf("Error: Division by zero.\n");   // Checks for error by 0
         return 0;
     }
 }
